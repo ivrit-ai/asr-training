21ec070a96933f03fc4d995db82fb72f07dab153
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-06-15 03:39:00 +0300

    Update with_faster_whisper.py

--- examples/with_faster_whisper.py
+++ examples/with_faster_whisper.py
@@ -3,6 +3,14 @@ Example using faster-whisper with ctranslate2 backend for fast audio transcripti
 Run with:
     pip install faster-whisper
     python with_faster_whisper.py
+    
+Usage on Ubuntu 22.04 with CUDA:
+1. Use python 3.11
+2. Install CudNN 9.1.0 with:
+    pip install faster-whisper==1.1.1
+    wget https://developer.download.nvidia.com/compute/cudnn/9.1.0/local_installers/cudnn-local-repo-ubuntu2204-9.1.0_1.0-1_amd64.deb
+    sudo dpkg -i cudnn-local-repo-ubuntu2204-9.1.0_1.0-1_amd64.deb && sudo cp /var/cudnn-local-repo-ubuntu2204-9.1.0/cudnn-*-keyring.gpg /usr/share/keyrings/
+    sudo apt-get update && sudo apt-get -y install cudnn
 """
 
 import faster_whisper
